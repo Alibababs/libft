e2f17742b7fa3f6f6f00035d4dfa7cb75aea159f
Alibababs <155173056+Alibababs@users.noreply.github.com>
2024-05-26 19:38:33 +0200

    Add files via upload

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbailly <pbailly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/26 18:25:55 by pbailly           #+#    #+#             */
-/*   Updated: 2024/05/26 18:54:20 by pbailly          ###   ########.fr       */
+/*   Created: 2024/05/26 18:55:30 by pbailly           #+#    #+#             */
+/*   Updated: 2024/05/26 19:05:24 by pbailly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
+	size_t	len;
 	size_t	i;
 	size_t	j;
 
-	i = 0;
-	j = 0;
-	if (start + len > ft_strlen(s))
-		len = ft_strlen(s) - start;
-	if (ft_strlen(s) < start)
-		len = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
 	str = malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
-	while (s[i])
+	i = 0;
+	while (s1[i])
 	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+		str[i] = s1[i];
 		i++;
 	}
-	str[j] = '\0';
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
 	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,41 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbailly <pbailly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/26 18:25:55 by pbailly           #+#    #+#             */
-/*   Updated: 2024/05/26 18:54:20 by pbailly          ###   ########.fr       */
+/*   Created: 2024/05/26 19:08:59 by pbailly           #+#    #+#             */
+/*   Updated: 2024/05/26 19:36:25 by pbailly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+static int	trim(char s, const char *set)
 {
-	char	*str;
 	size_t	i;
-	size_t	j;
 
 	i = 0;
-	j = 0;
-	if (start + len > ft_strlen(s))
-		len = ft_strlen(s) - start;
-	if (ft_strlen(s) < start)
-		len = 0;
+	while (set[i])
+	{
+		if (s == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
+	size_t	x;
+	size_t	len;
+	char	*str;
+
+	i = 0;
+	while (trim(s1[i], set))
+		i++;
+	j = ft_strlen(s1);
+	while (trim(s1[j], set))
+		j--;
+	len = j - i;
 	str = malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
-	while (s[i])
+	x = 0;
+	while (i < len)
 	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+		str[x] = s1[i];
+		x++;
 		i++;
 	}
-	str[j] = '\0';
+	str[x] = '\0';
 	return (str);
 }
