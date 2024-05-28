732b04ea32bb4ddbaa1d39d67a3b01d20bfc19a5
Alibababs <alibaba@Alix>
2024-05-28 13:04:09 +0200

    lstmap perfect

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alibaba <alibaba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:44:18 by alibaba           #+#    #+#             */
-/*   Updated: 2024/05/28 12:37:57 by alibaba          ###   ########.fr       */
+/*   Updated: 2024/05/28 13:03:06 by alibaba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_cont;
+	void	*cont;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_cont = ft_lstnew(f(lst->content));
+		cont = f(lst->content);
+		new_cont = ft_lstnew(cont);
 		if (!new_cont)
 		{
+			del(cont);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
